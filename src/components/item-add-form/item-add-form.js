570ebd21ba2925af8b194f65c576bb0a44c7e780
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-12-25 21:48:58 +0300

    Adding an element with a form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item-add-form.css';
 
-const ItemAddForm = ({onAddItem}) => {
-    return (
-        <div className="item-add-form">
-            <button 
-                className="btn btn-outline-secondary"
-                onClick={() => onAddItem('new Item')}>
-                    Add item
-            </button>
-        </div>
-    );
-}
+export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    }
 
-export default ItemAddForm;
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (this.state.label) {
+            this.props.onAddItem(this.state.label);
+
+            this.setState({
+                label: ''
+            });
+        }
+
+    }
+
+    render() {
+        return (
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text"
+                    className='form-control mr-1'
+                    onChange={this.onLabelChange}
+                    placeholder="Whats needs to be done"
+                    value={this.state.label} />
+                <button className="btn btn-outline-secondary">Add item</button>
+            </form>
+        );
+    }
+}
